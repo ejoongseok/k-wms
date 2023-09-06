2fc7b5eccf67ed4828c035b5dc2506a4cc6f3044
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 16:37:27 +0900

    Integration setting.

---
+++ src/test/java/com/example/kwms/common/ApiTest.java
@@ -0,0 +1,22 @@
+package com.example.kwms.common;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        databaseCleaner.afterPropertiesSet();
+        databaseCleaner.execute();
+    }
+}
