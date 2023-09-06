ec9fce6b7470253f7b5738ab367e69906720cd71
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 17:09:16 +0900

    add request Request#Product.

--- src/test/java/com/example/kwms/inbound/CreateInboundTest.java
+++ src/test/java/com/example/kwms/inbound/CreateInboundTest.java
@@ -1,5 +1,6 @@
 package com.example.kwms.inbound;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 class CreateInboundTest {
 
@@ -20,16 +22,20 @@ class CreateInboundTest {
     @Test
     @DisplayName("입고 생성")
     void createInbound() {
-        final String title = "블랙핑크 앨범 입고";
-        final LocalDateTime estimatedArrivalAt = LocalDateTime.now().plusDays(1L);
-        final LocalDateTime orderRequestedAt = LocalDateTime.now();
-        final String description = "23년도 블랙핑크 신규 앨범 주문";
+        final CreateInbound.Request.Product product = new CreateInbound.Request.Product(
+                1L,
+                2_000L,
+                15_000L,
+                "블랙핑크 3집 앨범[]"
+        );
 
         final CreateInbound.Request request = new CreateInbound.Request(
-                title,
-                estimatedArrivalAt,
-                orderRequestedAt,
-                description);
+                "블랙핑크 앨범 입고",
+                LocalDateTime.now().plusDays(1L),
+                LocalDateTime.now(),
+                "23년도 블랙핑크 신규 앨범 주문",
+                List.of(product));
+
 
         createInbound.request(request);
 
@@ -48,7 +54,19 @@ class CreateInboundTest {
                 LocalDateTime estimatedArrivalAt,
                 @NotNull(message = "주문 요청일은 필수입니다.")
                 LocalDateTime orderRequestedAt,
-                String description) {
+                String description,
+                List<Product> inboundProducts) {
+            public record Product(
+                    @NotNull(message = "상품 번호는 필수입니다.")
+                    Long productNo,
+                    @NotNull(message = "상품 입고 요청 수량은 필수입니다.")
+                    @Min(value = 1, message = "상품 입고 요청 수량은 1개 이상이어야 합니다.")
+                    Long requestQuantity,
+                    @NotNull(message = "상품 입고 요청 단가는 필수입니다.")
+                    @Min(value = 0, message = "상품 입고 요청 단가는 0원 이상이어야 합니다.")
+                    Long unitPrice,
+                    String description) {
+            }
         }
     }
 }
