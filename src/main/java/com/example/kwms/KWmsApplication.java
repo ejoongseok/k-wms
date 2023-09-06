9ae7a7add2927396648a9f19cf673d2d7e56e28e
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 16:31:13 +0900

    Initial commit

---
+++ src/main/java/com/example/kwms/KWmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KWmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwms/KWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
