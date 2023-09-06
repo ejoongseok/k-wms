161c69d9ae58dea32cd1fd76ca71a07757e78c62
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 16:47:21 +0900

    create inner class: CreateInbound.

--- src/test/java/com/example/kwms/inbound/CreateInboundTest.java
+++ src/test/java/com/example/kwms/inbound/CreateInboundTest.java
@@ -1,12 +1,24 @@
 package com.example.kwms.inbound;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CreateInboundTest {
+
+    private CreateInbound createInbound;
+
+    @BeforeEach
+    void setUp() {
+        createInbound = new CreateInbound();
+    }
+
     @Test
     @DisplayName("입고 생성")
     void createInbound() {
 
+    }
+
+    private class CreateInbound {
     }
 }
