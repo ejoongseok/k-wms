8b1dba77cfada56ab6cdf69414c1d6b0513e27b0
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 16:45:57 +0900

    create test case: createInbound.

---
+++ src/test/java/com/example/kwms/inbound/CreateInboundTest.java
@@ -0,0 +1,12 @@
+package com.example.kwms.inbound;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreateInboundTest {
+    @Test
+    @DisplayName("입고 생성")
+    void createInbound() {
+
+    }
+}
