ee3f63c4b2f0002f7912350ec54cb5a74b9d0979
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-06 17:15:08 +0900

    입고 도메인 추가 & request -> 입고 도메인 매핑 코드 추가.

--- src/test/java/com/example/kwms/inbound/CreateInboundTest.java
+++ src/test/java/com/example/kwms/inbound/CreateInboundTest.java
@@ -6,9 +6,12 @@ import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class CreateInboundTest {
 
@@ -42,9 +45,68 @@ class CreateInboundTest {
         // inboundRepository.findById(1L).asn();
     }
 
+    private static class InboundProduct {
+        private final Long productNo;
+        private final Long requestQuantity;
+        private final Long unitPrice;
+        private final String description;
+        private Inbound inbound;
+
+        public InboundProduct(
+                final Long productNo,
+                final Long requestQuantity,
+                final Long unitPrice,
+                final String description) {
+            Assert.notNull(productNo, "상품 번호는 필수입니다.");
+            Assert.notNull(requestQuantity, "상품 입고 요청 수량은 필수입니다.");
+            if (1 > requestQuantity)
+                throw new IllegalArgumentException("상품 입고 요청 수량은 1개 이상이어야 합니다.");
+            Assert.notNull(unitPrice, "상품 입고 요청 단가는 필수입니다.");
+            if (0 > unitPrice)
+                throw new IllegalArgumentException("상품 입고 요청 단가는 0원 이상이어야 합니다.");
+
+            this.productNo = productNo;
+            this.requestQuantity = requestQuantity;
+            this.unitPrice = unitPrice;
+            this.description = description;
+        }
+
+        public void assignInbound(final Inbound inbound) {
+            Assert.notNull(inbound, "입고는 필수입니다.");
+            this.inbound = inbound;
+        }
+    }
+
+    private static class Inbound {
+        private final String title;
+        private final LocalDateTime estimatedArrivalAt;
+        private final LocalDateTime orderRequestedAt;
+        private final String description;
+        private List<InboundProduct> inboundProducts = new ArrayList<>();
+
+        public Inbound(
+                final String title,
+                final LocalDateTime estimatedArrivalAt,
+                final LocalDateTime orderRequestedAt,
+                final String description,
+                final List<InboundProduct> inboundProducts) {
+            Assert.hasText(title, "입고 제목은 필수입니다.");
+            Assert.notNull(estimatedArrivalAt, "입고 예정일은 필수입니다.");
+            Assert.notNull(orderRequestedAt, "주문 요청일은 필수입니다.");
+            Assert.notNull(inboundProducts, "입고 상품은 필수입니다.");
+            this.title = title;
+            this.estimatedArrivalAt = estimatedArrivalAt;
+            this.orderRequestedAt = orderRequestedAt;
+            this.description = description;
+            this.inboundProducts = inboundProducts;
+            inboundProducts.forEach(product -> product.assignInbound(this));
+        }
+
+    }
+
     private class CreateInbound {
         public void request(final Request request) {
-            throw new UnsupportedOperationException("Unsupported request");
+            final Inbound inbound = request.toDomain();
         }
 
         public record Request(
@@ -56,6 +118,17 @@ class CreateInboundTest {
                 LocalDateTime orderRequestedAt,
                 String description,
                 List<Product> inboundProducts) {
+            public Inbound toDomain() {
+                return new Inbound(
+                        title,
+                        estimatedArrivalAt,
+                        orderRequestedAt,
+                        description,
+                        inboundProducts.stream()
+                                .map(Product::toDomain)
+                                .collect(Collectors.toList()));
+            }
+
             public record Product(
                     @NotNull(message = "상품 번호는 필수입니다.")
                     Long productNo,
@@ -66,6 +139,13 @@ class CreateInboundTest {
                     @Min(value = 0, message = "상품 입고 요청 단가는 0원 이상이어야 합니다.")
                     Long unitPrice,
                     String description) {
+                public InboundProduct toDomain() {
+                    return new InboundProduct(
+                            productNo,
+                            requestQuantity,
+                            unitPrice,
+                            description);
+                }
             }
         }
     }
