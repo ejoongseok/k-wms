efc40d8b83bf3756e9e8da796e0c042c5b2d56b9
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-09 10:25:45 +0900

    입고상태 제거.

--- src/main/java/com/example/kwms/inbound/domain/Inbound.java
+++ src/main/java/com/example/kwms/inbound/domain/Inbound.java
@@ -3,7 +3,6 @@ package com.example.kwms.inbound.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,11 +39,6 @@ public class Inbound {
     private String description;
     @OneToMany(mappedBy = "inbound", orphanRemoval = true, cascade = CascadeType.ALL)
     private final List<InboundProduct> inboundProducts = new ArrayList<>();
-    @Enumerated
-    @Column(name = "status", nullable = false)
-    @Comment("입고 상태")
-    private InboundStatus status;
-
 
     public Inbound(
             final String title,
@@ -59,7 +53,6 @@ public class Inbound {
         this.estimatedArrivalAt = estimatedArrivalAt;
         this.orderRequestedAt = orderRequestedAt;
         this.description = description;
-        status = InboundStatus.ORDER_REQUESTED;
     }
 
     public void assignProducts(final List<InboundProduct> products) {
