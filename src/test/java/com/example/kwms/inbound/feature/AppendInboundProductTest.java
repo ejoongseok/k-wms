a5759bf8c161c90200a380c84f834c8f7a4c288b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-09 10:30:36 +0900

    add parameter.

--- src/test/java/com/example/kwms/inbound/feature/AppendInboundProductTest.java
+++ src/test/java/com/example/kwms/inbound/feature/AppendInboundProductTest.java
@@ -1,5 +1,7 @@
 package com.example.kwms.inbound.feature;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +19,17 @@ class AppendInboundProductTest {
     @Test
     @DisplayName("입고 상품을 추가한다.")
     void appendInboundProduct() {
-        final AppendInboundProduct.Request request = new AppendInboundProduct.Request();
+        final Long inboundNo = 1L;
+        final AppendInboundProduct.Request.Product product = new AppendInboundProduct.Request.Product(
+                1L,
+                1_000L,
+                15_000L,
+                "블랙핑크 3집 앨범[] - 미입고분 추가 입고"
+        );
+        final AppendInboundProduct.Request request = new AppendInboundProduct.Request(
+                inboundNo,
+                product
+        );
         appendInboundProduct.request(request);
     }
 
@@ -26,7 +38,23 @@ class AppendInboundProductTest {
             throw new UnsupportedOperationException("Unsupported request");
         }
 
-        public record Request() {
+        public record Request(
+                @NotNull(message = "입고 번호는 필수입니다.")
+                Long inboundNo,
+                @NotNull(message = "입고 상품은 필수입니다.")
+                Request.Product product) {
+            public record Product(
+                    @NotNull(message = "상품 번호는 필수입니다.")
+                    Long productNo,
+                    @NotNull(message = "상품 입고 요청 수량은 필수입니다.")
+                    @Min(value = 1, message = "상품 입고 요청 수량은 1개 이상이어야 합니다.")
+                    Long requestQuantity,
+                    @NotNull(message = "상품 입고 요청 단가는 필수입니다.")
+                    @Min(value = 0, message = "상품 입고 요청 단가는 0원 이상이어야 합니다.")
+                    Long unitPrice,
+                    String description
+            ) {
+            }
         }
     }
 }
