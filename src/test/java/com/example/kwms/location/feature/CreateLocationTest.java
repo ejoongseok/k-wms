31484bb6c99c227150c7634b3a9965fc1e67b48a
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-10 16:37:51 +0900

    로케이션 생성.

---
+++ src/test/java/com/example/kwms/location/feature/CreateLocationTest.java
@@ -0,0 +1,26 @@
+package com.example.kwms.location.feature;
+
+import com.example.kwms.common.ApiTest;
+import com.example.kwms.common.Scenario;
+import com.example.kwms.location.domain.LocationRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CreateLocationTest extends ApiTest {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+
+    @Test
+    @DisplayName("로케이션을 생성한다.")
+    void createLocation() {
+        Scenario.createLocation().request();
+
+        assertThat(locationRepository.findAll()).hasSize(1);
+    }
+
+}
