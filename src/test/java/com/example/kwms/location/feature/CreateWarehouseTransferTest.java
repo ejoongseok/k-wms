78319897e2823149bf0b1a4c63ebf6a2482ec616
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-11 08:37:50 +0900

    .

--- src/test/java/com/example/kwms/location/feature/CreateWarehouseTransferTest.java
+++ src/test/java/com/example/kwms/location/feature/CreateWarehouseTransferTest.java
@@ -1,5 +1,6 @@
 package com.example.kwms.location.feature;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,12 @@ public class CreateWarehouseTransferTest {
     @Test
     @DisplayName("창고간 재고 이동을 생성한다.")
     void createWarehouseTransfer() {
+        final Long productNo = 1L;
+        final Long quantity = 1L;
+        final CreateWarehouseTransfer.Request.Product product = new CreateWarehouseTransfer.Request.Product(
+                productNo,
+                quantity
+        );
         final Long fromWarehouseNo = 1L;
         final Long toWarehouseNo = 2L;
         final String barcode = "WT-001";
@@ -42,6 +49,13 @@ public class CreateWarehouseTransferTest {
                 Long toWarehouseNo,
                 @NotBlank(message = "바코드는 필수입니다.")
                 String barcode) {
+            public record Product(
+                    @NotNull(message = "상품 번호는 필수입니다.")
+                    Long productNo,
+                    @NotNull(message = "수량은 필수입니다.")
+                    @Min(value = 1, message = "수량은 1보다 커야 합니다.")
+                    Long quantity) {
+            }
         }
     }
 }
