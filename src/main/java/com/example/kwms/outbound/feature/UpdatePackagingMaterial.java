24bdb50ed6fe553115656686433cb1e9a862e756
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-12 00:07:37 +0900

    포장재 수정.

---
+++ src/main/java/com/example/kwms/outbound/feature/UpdatePackagingMaterial.java
@@ -0,0 +1,97 @@
+package com.example.kwms.outbound.feature;
+
+import com.example.kwms.outbound.domain.MaterialType;
+import com.example.kwms.outbound.domain.PackagingMaterial;
+import com.example.kwms.outbound.domain.PackagingMaterialDimension;
+import com.example.kwms.outbound.domain.PackagingMaterialRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UpdatePackagingMaterial {
+
+    private final PackagingMaterialRepository packagingMaterialRepository;
+
+    @PatchMapping("/packaging-materials/{packagingMaterialNo}")
+    @Transactional
+    public void request(
+            @PathVariable final Long packagingMaterialNo,
+            @RequestBody @Valid final Request request) {
+        final PackagingMaterial packagingMaterial = packagingMaterialRepository.getBy(packagingMaterialNo);
+
+        packagingMaterial.update(
+                request.name(),
+                request.code(),
+                new PackagingMaterialDimension(
+                        request.innerWidthInMillimeters(),
+                        request.innerHeightInMillimeters(),
+                        request.innerLengthInMillimeters(),
+                        request.outerWidthInMillimeters(),
+                        request.outerHeightInMillimeters(),
+                        request.outerLengthInMillimeters()
+                ),
+                request.weightInGrams(),
+                request.maxWeightInGrams(),
+                request.materialType());
+    }
+
+    public record Request(
+            @NotBlank(message = "포장재 이름은 필수입니다.")
+            String name,
+            @NotBlank(message = "포장재 코드는 필수입니다.")
+            String code,
+            @NotNull(message = "내부 폭은 필수입니다.")
+            @Min(value = 1, message = "내부 폭은 1mm 이상이어야 합니다.")
+            Long innerWidthInMillimeters,
+            @NotNull(message = "내부 높이는 필수입니다.")
+            @Min(value = 1, message = "내부 높이는 1mm 이상이어야 합니다.")
+            Long innerHeightInMillimeters,
+            @NotNull(message = "내부 길이는 필수입니다.")
+            @Min(value = 1, message = "내부 길이는 1mm 이상이어야 합니다.")
+            Long innerLengthInMillimeters,
+            @NotNull(message = "외부 폭은 필수입니다.")
+            @Min(value = 1, message = "외부 폭은 1mm 이상이어야 합니다.")
+            Long outerWidthInMillimeters,
+            @NotNull(message = "외부 높이는 필수입니다.")
+            @Min(value = 1, message = "외부 높이는 1mm 이상이어야 합니다.")
+            Long outerHeightInMillimeters,
+            @NotNull(message = "외부 길이는 필수입니다.")
+            @Min(value = 1, message = "외부 길이는 1mm 이상이어야 합니다.")
+            Long outerLengthInMillimeters,
+            @NotNull(message = "무게는 필수입니다.")
+            @Min(value = 1, message = "무게는 1g 이상이어야 합니다.")
+            Long weightInGrams,
+            @NotNull(message = "최대 무게는 필수입니다.")
+            @Min(value = 1, message = "최대 무게는 1g 이상이어야 합니다.")
+            Long maxWeightInGrams,
+            @NotNull(message = "포장재 타입은 필수입니다.")
+            MaterialType materialType
+    ) {
+        public PackagingMaterial toDomain() {
+            return new PackagingMaterial(
+                    name,
+                    code,
+                    new PackagingMaterialDimension(
+                            innerWidthInMillimeters,
+                            innerHeightInMillimeters,
+                            innerLengthInMillimeters,
+                            outerWidthInMillimeters,
+                            outerHeightInMillimeters,
+                            outerLengthInMillimeters
+                    ),
+                    weightInGrams,
+                    maxWeightInGrams,
+                    materialType
+            );
+        }
+    }
+}
