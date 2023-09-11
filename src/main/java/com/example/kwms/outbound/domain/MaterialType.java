55d004da0e1edbb58461ef7af02d649a863f33ad
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-11 23:56:57 +0900

    포장재 등록.

---
+++ src/main/java/com/example/kwms/outbound/domain/MaterialType.java
@@ -0,0 +1,11 @@
+package com.example.kwms.outbound.domain;
+
+public enum MaterialType {
+    CORRUGATED_BOX("골판지 상자");
+
+    private final String description;
+
+    MaterialType(final String description) {
+        this.description = description;
+    }
+}
