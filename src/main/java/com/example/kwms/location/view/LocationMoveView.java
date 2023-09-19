82f8ccdfcf918f02ba98753c0f819521a04bc80c
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-09-20 05:42:54 +0900

    로케이션 이동 화면 추가.

---
+++ src/main/java/com/example/kwms/location/view/LocationMoveView.java
@@ -0,0 +1,28 @@
+package com.example.kwms.location.view;
+
+import com.example.kwms.location.domain.Location;
+import com.example.kwms.location.domain.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class LocationMoveView {
+    private final LocationRepository locationRepository;
+
+    @GetMapping("/web/locations/{locationNo}/move-location")
+    @Transactional(readOnly = true)
+    public String moveLocation(
+            @PathVariable final Long locationNo,
+            final Model model) {
+        final Location location = locationRepository.getBy(locationNo);
+        model.addAttribute("locationBarcode", location.getLocationBarcode());
+        model.addAttribute("locationNo", location.getLocationNo());
+        return "location/move-location";
+    }
+
+}
