b4ce3f07cf10fbfb329f44c37e6df23dae3c7dca
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-10-15 21:19:25 +0900

    convert record to class.

--- src/main/java/com/example/kwms/inbound/feature/query/PurchaseOrderPresenter.java
+++ src/main/java/com/example/kwms/inbound/feature/query/PurchaseOrderPresenter.java
@@ -1,6 +1,18 @@
 package com.example.kwms.inbound.feature.query;
 
 import com.example.kwms.inbound.domain.PurchaseOrder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
-public record PurchaseOrderPresenter(Long purchaseOrderProductNo, PurchaseOrder purchaseOrder) {
+@Getter
+@Accessors(fluent = true)
+public final class PurchaseOrderPresenter {
+    private final Long purchaseOrderProductNo;
+    private final PurchaseOrder purchaseOrder;
+
+    PurchaseOrderPresenter(final Long purchaseOrderProductNo, final PurchaseOrder purchaseOrder) {
+        this.purchaseOrderProductNo = purchaseOrderProductNo;
+        this.purchaseOrder = purchaseOrder;
+    }
+
 }
