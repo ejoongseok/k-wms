951800b5818f7583c4525e503eba021c387e8661
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-10-15 21:22:37 +0900

    move instance method.

--- src/main/java/com/example/kwms/inbound/feature/query/GetPurchaseOrders.java
+++ src/main/java/com/example/kwms/inbound/feature/query/GetPurchaseOrders.java
@@ -1,9 +1,7 @@
 package com.example.kwms.inbound.feature.query;
 
-import com.example.kwms.common.NotFoundException;
 import com.example.kwms.inbound.domain.LPN;
 import com.example.kwms.inbound.domain.PurchaseOrder;
-import com.example.kwms.inbound.domain.PurchaseOrderProduct;
 import com.example.kwms.inbound.domain.PurchaseOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -32,20 +30,9 @@ public class GetPurchaseOrders {
                 .toList();
     }
 
-    private List<LPN> getLPNs(final PurchaseOrderPresenter purchaseOrderPresenter) {
-        final List<PurchaseOrderProduct> purchaseOrderProducts = purchaseOrderPresenter.purchaseOrder().getPurchaseOrderProducts();
-        final PurchaseOrderProduct purchaseOrderProduct = purchaseOrderProducts.stream()
-                .filter(product -> product.getProductNo().equals(purchaseOrderPresenter.purchaseOrderProductNo()))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundException(
-                        "발주 상품 번호에 해당하는 발주 상품이 존재하지 않습니다. 상품 번호: %s".formatted(purchaseOrderPresenter.purchaseOrderProductNo())));
-
-        return purchaseOrderProduct.getLpns();
-    }
-
     private List<LPN> getLpns(final Long purchaseOrderNo, final Long purchaseOrderProductNo) {
         final PurchaseOrder purchaseOrder = purchaseOrderRepository.getBy(purchaseOrderNo);
-        return getLPNs(new PurchaseOrderPresenter(purchaseOrderProductNo, purchaseOrder));
+        return new PurchaseOrderPresenter(purchaseOrderProductNo, purchaseOrder).getLPNs();
     }
 
     private record LPNResponse(
--- src/main/java/com/example/kwms/inbound/feature/query/PurchaseOrderPresenter.java
+++ src/main/java/com/example/kwms/inbound/feature/query/PurchaseOrderPresenter.java
@@ -1,8 +1,13 @@
 package com.example.kwms.inbound.feature.query;
 
+import com.example.kwms.common.NotFoundException;
+import com.example.kwms.inbound.domain.LPN;
 import com.example.kwms.inbound.domain.PurchaseOrder;
+import com.example.kwms.inbound.domain.PurchaseOrderProduct;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+
+import java.util.List;
 
 @Getter
 @Accessors(fluent = true)
@@ -15,4 +20,14 @@ public final class PurchaseOrderPresenter {
         this.purchaseOrder = purchaseOrder;
     }
 
+    List<LPN> getLPNs() {
+        final List<PurchaseOrderProduct> purchaseOrderProducts = purchaseOrder().getPurchaseOrderProducts();
+        final PurchaseOrderProduct purchaseOrderProduct = purchaseOrderProducts.stream()
+                .filter(product -> product.getProductNo().equals(purchaseOrderProductNo()))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException(
+                        "발주 상품 번호에 해당하는 발주 상품이 존재하지 않습니다. 상품 번호: %s".formatted(purchaseOrderProductNo())));
+
+        return purchaseOrderProduct.getLpns();
+    }
 }
